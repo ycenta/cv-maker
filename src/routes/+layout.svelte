09395c67fc874d1ab87341940ea7a8a32d241e34
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-08-23 14:52:43 +0200

    fix(build): fixed dayjs error preventing website to be funtionnal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,9 @@
 <script>
-	import { browser } from '$app/environment';
 	import '../app.css';
-	import * as dayjs from 'dayjs';
+	import dayjs from 'dayjs';
 	import 'dayjs/locale/fr';
 
 	dayjs.locale('fr');
-
-	if (browser) {
-		if (
-			localStorage.getItem('color-theme') === 'dark' ||
-			(!('color-theme' in localStorage) &&
-				window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	}
 </script>
 
 <slot />
