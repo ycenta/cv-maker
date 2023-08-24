a5042f08ba225b69dad35ac4a8c78214e8ff8d62
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-08-24 17:30:30 +0200

    refactor(home.svelte): extracted common en & fr page code into a component

---
+++ src/lib/component/home.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Contact, { type IContactInfo } from '$lib/component/contact.svelte';
+	import Education, { type IEducation } from '$lib/component/education.svelte';
+	import Experience, { type IExperience } from '$lib/component/experience.svelte';
+	import Hobby, { type IHobby } from '$lib/component/hobby.svelte';
+	import Lang, { type ILang } from '$lib/component/lang.svelte';
+	import Options from '$lib/component/options.svelte';
+	import Skill, { type ISkill } from '$lib/component/skill.svelte';
+	import Title, { type ITitle } from '$lib/component/title.svelte';
+	import { experiences } from '../../store';
+
+
+  export let  experience: IExperience[];
+  export let  education: IEducation[];
+  export let  langs: ILang[];
+  export let  hobbies: IHobby[];
+  export let  title: ITitle;
+  export let  contactInfo: IContactInfo;
+  export let  skills: ISkill[];
+
+	$: experiences.set(experience);
+
+</script>
+
+<div class="py-0 dark:bg-base-100 shadow-sm rounded-sm navbar">
+	<Title {title} />
+	<div class="flex flex-col sm:flex-row w-full">
+		<Contact {contactInfo} />
+		<div class="self-end">
+			<Options />
+		</div>
+	</div>
+</div>
+<div class="hero grow pt-1 pb-2">
+	<div class="text-center lg:text-left desktop-grid w-screen h-full place-items-center gap-2">
+		<!-- <div style="grid-area: picture;">
+			<img src="/photo_cv.jpg" alt="Mohammad-Amine Banaei" class="mask mask-squircle w-32" />
+		</div> -->
+		<div class="my-2 flex justify-center self-start w-full" style="grid-area: skill;">
+			<Skill {skills} />
+		</div>
+		<div class="px-2 self-start w-full" style="grid-area: experience;">
+			<Experience experience={$experiences} />
+		</div>
+		<div class="divider sm:hidden mx-2 my-1"></div>
+		<div class="px-2 self-start w-full" style="grid-area: education;">
+			<Education {education} />
+			<div class="divider my-1"></div>
+			<Lang {langs} />
+			<div class="divider my-1"></div>
+			<Hobby {hobbies} />
+			<div class="mb-4 lg:mb-0"></div>
+		</div>
+	</div>
+</div>
+
+<style>
+	@media (min-width: 1024px) {
+		.desktop-grid {
+			display: grid;
+			grid-template-columns: 30% auto;
+			grid-template-rows: min-content auto;
+			grid-column-gap: 0px;
+			grid-row-gap: 0px;
+
+			grid-template-areas:
+				'skill skill'
+				'education experience';
+		}
+	}
+</style>
--- src/routes/en/+page.svelte
+++ src/routes/en/+page.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
-	import Contact from '$lib/component/contact.svelte';
-	import Education from '$lib/component/education.svelte';
-	import Experience from '$lib/component/experience.svelte';
-	import Hobby from '$lib/component/hobby.svelte';
-	import Lang from '$lib/component/lang.svelte';
-	import Options from '$lib/component/options.svelte';
+	import Home from '$lib/component/home.svelte';
 	import HeadTags from '$lib/component/seo/HeadTags.svelte';
-	import Skill from '$lib/component/skill.svelte';
-	import Title from '$lib/component/title.svelte';
 	import type { IMetaTagProperties } from '$lib/seo';
-	import { experiences } from '../../store';
-
+	
 	export let data: import('./$types').PageData;
-
-	$: experiences.set(data.experience);
 
 	let metaData: Partial<IMetaTagProperties> = {
 		title: data.contactInfo.name + ' - Resume',
@@ -30,50 +20,5 @@
 
 <HeadTags {metaData} />
 
-<div class="py-0 dark:bg-base-100 shadow-sm rounded-sm navbar">
-	<Title {...data} />
-	<div class="flex flex-col sm:flex-row w-full">
-		<Contact {...data} />
-		<div class="self-end">
-			<Options />
-		</div>
-	</div>
-</div>
-<div class="hero grow pt-1">
-	<div class="text-center lg:text-left desktop-grid w-screen h-full place-items-center gap-2">
-		<!-- <div style="grid-area: picture;">
-			<img src="/photo_cv.jpg" alt="Mohammad-Amine Banaei" class="mask mask-squircle w-32" />
-		</div> -->
-		<div class="my-2 flex justify-center self-start w-full" style="grid-area: skill;">
-			<Skill {...data} />
-		</div>
-		<div class="px-2 self-start w-full" style="grid-area: experience;">
-			<Experience experience={$experiences} />
-		</div>
-		<div class="divider lg:hidden mx-2"></div>
-		<div class="px-2 self-start w-full" style="grid-area: education;">
-			<Education {...data} />
-			<div class="divider"></div>
-			<Lang {...data} />
-			<div class="divider"></div>
-			<Hobby {...data} />
-			<div class="mb-4 lg:mb-0"></div>
-		</div>
-	</div>
-</div>
+<Home {...data} />
 
-<style>
-	@media (min-width: 1024px) {
-		.desktop-grid {
-			display: grid;
-			grid-template-columns: 30% auto;
-			grid-template-rows: min-content auto;
-			grid-column-gap: 0px;
-			grid-row-gap: 0px;
-
-			grid-template-areas:
-				'skill skill'
-				'education experience';
-		}
-	}
-</style>
